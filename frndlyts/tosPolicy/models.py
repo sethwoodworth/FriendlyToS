95480809013791b8d874be6ebf0d26edf9f6bfb8
Seth Woodworth <seth@sethish.com>
2011-09-05 13:22:56 -0400

    adding models that represent a tos Policy

---
+++ frndlyts/tosPolicy/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+class Organization(models.Model):
+    """
+    Base model for a website.
+    """
+    name        = models.CharField(max_length=50)
+    url         = models.URLField()
+    created_at  = models.DateTimeField()            # time and date org was added to this list
+
+class PolicyDocument(models.Model):
+    """
+    Policy Document. Can be many documents per organization.
+    """
+    orgid           = models.ForeignKey('Organization')
+    documentPath    = models.CharField(max_length=255)      # this might just be a textfield
+    title           = models.CharField(max_length=100)      # What the org calls this document (privacy policy, terms of service, other)
+
+class PolicyVersion(models.Model):
+    """
+    Each recorded revision of a Policy document.
+    """
+    docid           = models.ForeignKey('PolicyDocument')
+    # I think we can get by following fkeys backwards, don't need bidirectional fkeys
+    #firstParagraph  = models.ForeignKey('PolicyParagraph')
+    dateAdded       = models.DateTimeField()                # when we saw/saved the policy update
+    checkSum        = models.CharField(max_length=100)      # longer than we need
+
+class PolicyParagraph(models.Model):
+    """
+    Each paragraph of the policy.
+    """
+    versionId       = models.ForeignKey('PolicyVersion')
+    # previous is the prev paragraph as you read the document
+    # null and blank if first paragraph
+    previous        = models.ForeignKey('PolicyParagraph', null=True, blank=True)
+    # ancestor is the last revision of the paragraph
+    # null and blank if first PolicyVersion of PolicyDocument
+    ancestor        = models.ForeignKey('PolicyParagraph', null=True, blank=True)
+    checkSum        = models.CharField(max_length=100)      # longer than we need
+
+class PolicySentence(models.Model):
+    """
+    Store each sentence of the policy, with references to PolicyVersion.
+    """
+    paragraphId     = models.ForeignKey('PolicyParagraph')  # must be owned by a Paragraph
+    previous        = models.ForeignKey('PolicySentence', null=True, blank=True)
+    text            = models.TextField()                    # The actual content! (finally)
+    checkSum        = models.CharField(max_length=100)      # longer than we need
+
